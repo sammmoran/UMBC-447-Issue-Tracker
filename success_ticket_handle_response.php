33ea0e77bb0bc567db5e96d25781907694125651
rook2093 <srmoran93@gmail.com>
2019-10-21 16:35:10 -0400

    Create success_ticket_handle_response.php

---
+++ success_ticket_handle_response.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+
+	Hello, <?php echo $_POST["ticketauthor"]?>.</br>
+	Thank you for providing your ticket, "<?php echo $_POST["tickettitle"]?>".</br>
+	Your Service Contact, <?php echo $_POST["servicecontact"]?>, will address this be in contact with you shortly.</br>
+
+</body>
+</html>
