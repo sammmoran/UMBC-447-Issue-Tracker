39447acbe30aa40a47b81cbda01b0148f552a86a
rook2093 <srmoran93@gmail.com>
2019-10-29 16:41:39 -0400

    Add files via upload

--- insert_tickets.php
+++ insert_tickets.php
@@ -1,22 +1,39 @@
 <?php 
 
+	/*	
+		----- DELETE THIS -----
+		Establish connection
+		- This should be removed becuase Kyle and James are going to handle this
+	*/
 	require('database.php');
 	
-	$title = $_POST['tickettitle'];
+	/*
+		Capture variables from newTicket.html file.
+		Organize data and send to SQL database.
+		Kyle and James will have SQL functions to handle SQL aspects.
+	*/
+	$title = $_POST['issueName'];
 	$status = "Open";
-	$service_agent = $_POST['servicecontact'];
-	$history = $_POST['prelim_comments'];
+	$service_agent = $_POST['contactInfo'];
+	$history = $_POST['description'];
 	
+	
+	/*
+		------ DELETE THIS ----
+		This will be handled by James and Kyle 
+	*/
 	$query = 'INSERT INTO dashboard (title,status,service_agent,history) VALUES (:title,:status,:service_agent,:history)';
 	$sqprep = $conn->prepare($query);
 	$sqexec = $sqprep->execute(array(":title"=>$title, ":status"=>$status, ":service_agent"=>$service_agent,":history"=>$history));
 	
+	
+	
+	/*Remove this if statement*/
 	if ($sqexec){
-		include('success_ticket_handle_response.php');		
+		echo "It worked";
 	}
+	/*Debugging statement*/
 	else
 		echo "Query failed";
-	
-	include('pulldata.php');
 
 ?>
