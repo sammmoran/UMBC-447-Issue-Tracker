2a376c37859ac6b29a580b25437a6b15e4788370
rook2093 <srmoran93@gmail.com>
2019-11-01 12:37:29 -0400

    Validate ticket input
    This file will validate the character limits for the ticket data and return output to the user if requirements are not met.

---
+++ inputErrorCheck.php
@@ -0,0 +1,35 @@
+<?php 
+
+
+
+	function validateNewTicket($title, $poc_name, $poc_email, $description){
+
+		$ret = "";
+
+		if (strlen($title) > 128){
+			$ret = $ret."Your Issue Name should be less than 128 characters \n";
+			$ret = $ret."<br>";
+		}
+		
+		if (strlen($poc_email) > 64){
+			$ret = $ret."Your email should be less than 64 characters \n";
+			$ret = $ret."<br>";	
+		}
+
+		if (strlen($poc_name) > 64){
+			$ret = $ret."Please keep your name less than 64 characters \n";
+			$ret = $ret."<br>";
+		}
+		
+		if (strlen($description) > 65000){
+			$ret = $ret."Please keep your comments concise (less than 65,000 characters \n";
+			$ret = $ret."<br>";
+		}
+
+		return $ret;
+		
+	}
+
+
+
+?>
