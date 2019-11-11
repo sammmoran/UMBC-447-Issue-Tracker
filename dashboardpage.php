ad91279ed8aa9be492b13f1ad23664b8fbed5d7b
barkx <srmoran93@gmail.com>
2019-11-11 16:16:54 -0500

    Dashboard now loads data

--- dashboardpage.php
+++ dashboardpage.php
@@ -57,21 +57,14 @@
 		<table>
 		<?php 
 		
+			require('DB_Query_Functions.php');
+		
+		
 			/*
 				---- DELETE THIS -----
 				Kyle and James will be providing the function for connecting to the database.
 			*/
-			$conn = mysqli_connect('localhost', 'root', '', 'issue_tracker');
-			if ($conn->connect_error){
-				die('Connection failed:'.$conn->connect_error);				
-			}			
-			$sql = 'SELECT * FROM dashboard';
-			$result = $conn->query($sql);
-			
-			
-			
-			
-			
+					
 			/*
 				Set up a table that will make use of the SQL query retrieved from the database.
 				This will display a dashboard with the ticket's id, title, status, service agent, and
@@ -91,7 +84,21 @@
             <th>Date</th>
 			<th>Comment History</th>
 			</tr>";
-            
+        
+			$store = get_all_tickets();
+			foreach($store as $ticket){
+				echo"<tr>";
+				echo "<th>" . $ticket['tid'] . "</th>";
+				echo "<th>" . $ticket['title'] . "</th>";
+				echo "<th>" . $ticket['status'] . "</th>";
+				echo "<th>" . $ticket['poc_name'] . "</th>";
+				echo "<th>" . $ticket['poc_email'] . "</th>";
+				echo "<th>" . $ticket['modified_date'] . "</th>";
+				echo"</tr>";
+			}
+
+
+		
             
             /*commented out because it wasnt working
 			while($row = mysqli_fetch_assoc($result)){
@@ -119,7 +126,7 @@
 			}
 			echo"</table>";	
             */
-			$conn->close();
+			//$conn->close();
 		?>
 	
 		</table>
