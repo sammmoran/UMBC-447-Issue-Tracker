1dca222b1264fba818bcbad98fd9c64d42f88207
castlek1 <31139507+castlek1@users.noreply.github.com>
2019-11-21 19:49:38 -0500

    Update insert_tickets.php

--- insert_tickets.php
+++ insert_tickets.php
@@ -13,26 +13,26 @@
 	
 	$valInput = validateNewTicket($title, $poc_name, $poc_email, $description);
 	if (strlen($valInput) > 1){
+		header("refresh:5;url=newTicket.html");
 		echo "----- ERROR ------ \n";
 		echo "<br>";
 		echo "Read the following error output \n";
 		echo "<br>";
 		echo $valInput;
-		header("refresh:5, url=newTicket.html");
 	}	
 	
 	// Pass to database
 	// Will return 0 if it works; -1 if fails
 	if (add_ticket($title, $poc_name, $poc_email, $description) == 0){
+		header("refresh:2;url=dashboardpage.php");		
 		echo "Your new ticket has been added! \n";
 		echo "<br>";
-		header("refresh:2, url=dashboardpage.php");		
 	}
 	else{
+	    header("refresh:5;url=newTicket.html");
 		echo "TICKET ADD FAILURE";
 		echo "<br>";
 		echo "Try Again.";
-		header("refresh:5, url=newTicket.html");
 	}
 
 ?>
